bccfd832bb3db05b523a5a4c3297dd2363c404f5
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 18:28:07 +0200

    Bettering

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ set version=%1
 set key=%2
 
 cd %~dp0
-dotnet build magic.lambda.library/magic.lambda.library.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.library/magic.library.csproj --configuration Release --source https://api.nuget.org/v3/index.json
 dotnet nuget push magic.library/bin/Release/magic.library.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
