d7b135fa3a3bfff714d260f64baa120021341328
Thomas Hansen <thomas@gaiasoul.com>
2020-08-28 11:05:19 +0300

    Updating version, getting ready for release

--- build.cmd
+++
@@ -1,7 +0,0 @@
-
-set version=%1
-set key=%2
-
-cd %~dp0
-dotnet build magic.library/magic.library.csproj --configuration Release --source https://api.nuget.org/v3/index.json
-dotnet nuget push magic.library/bin/Release/magic.library.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
