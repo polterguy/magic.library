e62d73498a5ea8afd25adb9843761c174ca967b3
Thomas Hansen <thomas@gaiasoul.com>
2021-05-31 20:52:42 +0300

    Renaming SignalR project

--- magic.library/Initializer.cs
+++ magic.library/Initializer.cs
@@ -207,13 +207,13 @@ namespace magic.library
                          * This allows individual installations to use cookies to transmit JWT tokens, which
                          * arguably is more secure.
                          *
-                         * Notice, we also need to allow for SignalR requests to authenticate using QUERY parameters,
+                         * Notice, we also need to allow for sockets requests to authenticate using QUERY parameters,
                          * at which point we set token to value from 'access_token' QUERY param.
                          */
                         var cookie = context.Request.Cookies["ticket"];
                         if (!string.IsNullOrEmpty(cookie))
                             context.Token = cookie;
-                        else if (context.HttpContext.Request.Path.StartsWithSegments("/signalr") && context.Request.Query.ContainsKey("access_token"))
+                        else if (context.HttpContext.Request.Path.StartsWithSegments("/sockets") && context.Request.Query.ContainsKey("access_token"))
                             context.Token = context.Request.Query["access_token"];
                         return Task.CompletedTask;
                     },
